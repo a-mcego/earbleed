5aa0ac17aeabd98719120c39bef1f40fc4598ffb
Angela McEgo <eve.loikkanen@gmail.com>
2024-03-31 16:11:15 +0300

    Audio compression algo start
    Doesn't work properly yet, I need to implement multi-channel support

--- earbleed.py
+++ earbleed.py
@@ -1,8 +1,75 @@
+import glob
+import json
+import lzma
 import numpy as np
+import scipy
+import sys
 import time
+import zlib
+
+LZMA_FILTERS = [
+    {"id": lzma.FILTER_LZMA2, "preset": 9 | lzma.PRESET_EXTREME},
+]
+
+def save_ebl(eblname, compressed_bytes, params):
+    # Convert params dictionary to JSON string
+    params_json = json.dumps(params)
+    params_bytes = params_json.encode('utf-8')
+    
+    with open(eblname, 'wb') as file:
+        file.write(b'EBLE')
+        file.write(len(params_bytes).to_bytes(8, byteorder='little'))
+        file.write(params_bytes)
+        file.write(len(compressed_bytes).to_bytes(8, byteorder='little'))
+        file.write(compressed_bytes)
+
+def load_ebl(eblname):
+    with open(eblname, 'rb') as file:
+        fourcc = file.read(4)
+        if fourcc != b'EBLE':
+            print("Error: not an EarBleed file. Expected fourcc code 'EBLE'.", file=sys.stderr)
+            sys.exit(1)
+        params_length = int.from_bytes(file.read(8), byteorder='little')
+        params_bytes = file.read(params_length)
+        params = json.loads(params_bytes.decode('utf-8'))
+        compressed_length = int.from_bytes(file.read(8), byteorder='little')
+        compressed_bytes = file.read(compressed_length)
+    
+    return compressed_bytes, params
+
+
+def compress_array(array):
+    compressed_data = lzma.compress(array.tobytes(), format=lzma.FORMAT_RAW, filters=LZMA_FILTERS)
+    return compressed_data
+
+    #binary_data = array.tobytes()
+    #compressed_data = zlib.compress(binary_data, level=9, wbits=-15)
+    #return compressed_data
+
+def decompress_array(compressed_data, dtype):
+    binary_data = lzma.decompress(compressed_data, format=lzma.FORMAT_RAW, filters=LZMA_FILTERS)
+    array = np.frombuffer(binary_data, dtype=dtype)
+    return array
+
+    #binary_data = zlib.decompress(compressed_data, wbits=-15)
+    #array = np.frombuffer(binary_data, dtype=dtype)
+    #return array
+
+
+def read_wave(filename, window_size):
+    samplerate,x = scipy.io.wavfile.read(filename) #get wave data
+    x = np.transpose(x)
+    x = x.astype(np.float32)
+    if x.shape[1]%window_size != 0:
+        x = np.concatenate([x, np.zeros((x.shape[0],window_size-x.shape[0]%window_size))], axis=-1)
+    x = np.average(x,axis=0,keepdims=True) #TODO: support stereo
+    x = x/32768.0
+    return x,samplerate
+
+def write_wave(filename, data, samplerate):
+    scipy.io.wavfile.write(filename, samplerate, (np.clip(data,-1.0,1.0)*32767.0).astype(np.int16))
 
 #implementation of MDCT
-
 def MDCTmatrix(N):
     nums = np.arange(2*N)+0.5
     ws = nums[:,None]/N
@@ -17,9 +84,6 @@ class MDCT:
         self.matrix = MDCTmatrix(self.N)
 
     def forward(self, x):
-        #TODO: do padding for non-multiple-of-window-size data!
-        #      for now, it only works with data that is a multiple of the window size!
-    
         #pad with zeros to get rid of edge artifacts
         x = np.concatenate([np.zeros(self.N), x, np.zeros(self.N)])
         L = len(x)
@@ -33,22 +97,49 @@ class MDCT:
         out = out[1:, :self.N] + out[:-1, self.N:]
         return out.flatten()
 
-WINDOW_SIZE = 8192
-TOTALSIZE = 512
-#TODO: load wave files or something?
-x = np.random.rand(TOTALSIZE * WINDOW_SIZE)*2.0-1.0  # Example input signal
 
-time1 = time.time()
+def compressDCT(X,multiplier):
+    X = np.round(X*multiplier)
+    print(np.min(X), np.max(X))
+    compressedX = compress_array(X.flatten().astype(np.int16))
+    return compressedX
+    
+def decompressDCT(compressedX,multiplier):
+    decompressedX = decompress_array(compressedX, np.int16)
+    decompressedX = decompressedX.astype(np.float32) / multiplier
+    decompressedX = np.reshape(decompressedX, (-1, WINDOW_SIZE))
+    return decompressedX
+
+
+MULTIPLIER = 17.3
+WINDOW_SIZE = 2048
+#wavename = "Kapittel_1.wav"
+wavename = "rule001.wav"
+x,samplerate = read_wave(wavename, WINDOW_SIZE)
+channels = x.shape[0]
 mdct = MDCT(WINDOW_SIZE)
-time2 = time.time()
 X = mdct.forward(x)
-time3 = time.time()
+originalsize = X.shape[1]*X.shape[2]
+X = compressDCT(X, MULTIPLIER)
+
+compressedsize = len(X)
+seconds = originalsize/samplerate
+bitrate = compressedsize*8/1000/seconds
+print(f"bitrate: {bitrate}kbps")
+
+eblname = wavename[:-4]+".ebl"
+params = {
+    'w':WINDOW_SIZE,
+    's':samplerate,
+    'c':channels,
+    'm':MULTIPLIER,
+    'b':bitrate,
+    'l':seconds
+}
+save_ebl(eblname, X, params)
+
+X,params = load_ebl(eblname)
+X = decompressDCT(X, params['m'])
 x_reconstructed = mdct.backward(X)
-time4 = time.time()
 
-print("Init: ", time2-time1)
-print("Forward: ", time3-time2)
-print("Backward: ", time4-time3)
-
-print(np.allclose(x, x_reconstructed, atol=1e-2), end=" ")
-
+write_wave("out.wav", x_reconstructed, samplerate)
